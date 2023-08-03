e8bfa1d46703f85cf9893eed9159913b76bb3c87
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-08-03 09:35:39 +0530

    Added actions() in App.js NewEvent Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventDetailPage, {
   loader as eventDetailLoader,
 } from "./pages/EventDetail";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -30,7 +30,7 @@ const router = createBrowserRouter([
           },
           {
             path: ":eventId",
-            id: 'event-detail',
+            id: "event-detail",
             loader: eventDetailLoader,
             children: [
               {
@@ -41,7 +41,7 @@ const router = createBrowserRouter([
             ],
           },
 
-          { path: "new", element: <NewEventPage /> },
+          { path: "new", element: <NewEventPage />, action: newEventAction },
         ],
       },
     ],
